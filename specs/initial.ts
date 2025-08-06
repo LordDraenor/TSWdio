3f048693a3d6d49e392760339bba8bcc5fc44c01
Lucian <lucian7stoicescu@gmail.com>
2025-08-06 18:21:00 -0400

    Upgrayd

--- specs/initial.ts
+++ specs/initial.ts
@@ -2,7 +2,7 @@
 
 describe("Check basic homepage navigation", function() {
     it("opens the webpage", async function() {
-        await browser.url('https://www.therapynotes.com');
+        await browser.url('https://qa-myaccount.nationalgrid.com');
         await browser.pause(5000);
     });
 
